7d1a1d8257b4632cbdde473f2eed2001e664ccfb
ShabanK <shabankhawar99@gmail.com>
2021-01-20 17:54:19 +0500

    created a card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './components/card/card.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule
