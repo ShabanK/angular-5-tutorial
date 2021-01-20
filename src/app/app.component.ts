2834ae6a5deaafad0af9bde3faf0756a1f0d7678
ShabanK <shabankhawar99@gmail.com>
2021-01-20 16:53:36 +0500

    app complete... css remaining

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { parse } from 'querystring';
 
 
 @Component({
@@ -7,12 +8,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  text: string;
-  translatedText: string;
+  password:string=""
+  includeLetters:boolean=false;  
+  includeNumbers:boolean=false;
+  includeSymbols:boolean=false;
+  passwordLength:number=0;
+
   constructor(){}
 
-  submit(){
-    
+  onChangeGetLength(length:string){
+    const parsedValue = parseInt(length)
+    if (!isNaN(parsedValue)){
+      this.passwordLength=parsedValue
+    }
   }
-
+  onChangeUseLetters(){
+    this.includeLetters=!this.includeLetters
+  }
+  onChangeUseNumbers(){
+    this.includeNumbers=!this.includeNumbers
+  }
+  onChangeUseSymbols(){
+    this.includeSymbols=!this.includeSymbols
+  }
+  onButtonClick(){
+    console.log("Button was pressed")
+    // console.log(this.includeLetters)
+    // console.log(this.includeNumbers)
+    // console.log(this.includeSymbols)
+    // console.log(this.passwordLength)
+    const numbers = "1234567890";
+    const letters = "qwertyuiopasdfghjklzxcvbnm"
+    const symbols = "!@#$%^&*()"
+    let validChars = ""
+    if (this.includeLetters) validChars+=letters;
+    if (this.includeNumbers) validChars+=numbers;
+    if (this.includeSymbols) validChars+=symbols;
+    let generatedPassword = ""
+    for (let i = 0; i<this.passwordLength; i++){
+      const index = Math.floor(Math.random()*validChars.length)
+      generatedPassword+=validChars[index]
+    }
+    this.password=generatedPassword
+  }
 }
