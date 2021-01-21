a2a88163fa7e469b5879ee9bcfdb34783beba9c9
ShabanK <shabankhawar99@gmail.com>
2021-01-21 14:10:31 +0500

    custom attr directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ClassDirective } from './directives/class.directive';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClassDirective
   ],
   imports: [
     BrowserModule
