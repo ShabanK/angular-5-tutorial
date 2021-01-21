46f5a30ac866d25e756b676446d89b9a78c318b5
ShabanK <shabankhawar99@gmail.com>
2021-01-21 12:44:25 +0500

    pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+  name:string;
+  date:string;
+  amount: number;
+  height: number;
+
+  car={
+    make: "Toyota",
+    model: "Camry",
+    year: 2000
+  }
+
+  onNameChange(name:string){
+    this.name=name;
+  }
+  onDateChange(date:string){
+    this.date = date
+  }
+  onAmountChange(amount:string){
+    this.amount = parseInt(amount)
+  }
+  onHeightChange(height:string){
+    this.height=parseFloat(height)
+  }
 }
